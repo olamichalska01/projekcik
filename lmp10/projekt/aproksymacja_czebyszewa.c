9b4c887dc80d280d9256c2ff31dcae2d657e0480
olamichalska01 <aleksandra.michalska01@gmail.com>
2021-01-15 19:09:45 +0100

    Dodanie funkcji obliczajcych 1, 2 i 3 pochodna

--- lmp10/projekt/aproksymacja_czebyszewa.c
+++ lmp10/projekt/aproksymacja_czebyszewa.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
+#include <math.h>
+
+
 
 double cze_n(int st, double x)
 {
-	if(st == 0) return 1;
-	else if( st == 1) return x;
-	else  return 2*x*cze_n(st-1,x)-cze_n(st-2,x);
+        if(st == 0) return 1;
+        else if( st == 1) return x;
+        else  return 2 * x * cze_n(st-1, x) - cze_n(st-2, x);
 }
 
 double d_czen_jeden(int st, double x)
 {
-	if(st == 0) return 0;
-	else if( st == 1) return 1;
-       	else
-	{
-		return (2*cze_n(st-1,x) + 2*x*d_czen_jeden(st-1,x)-d_czen_jeden(st-2,x);
-	}	
+        return 0.5 * st * ( pow( x + pow(x * x - 1, 0.5) , st - 1 ) * ( 1 + x * pow(x * x - 1, -0.5) ) + pow( x - pow(x * x - 1, 0.5) , st - 1 ) * ( 1 - x * pow(x * x - 1, -0.5) ) );
+
 }
 
-double d_czen_dwa(int st, double x)
+double d_czen_dwa(int st, double x) // jak ktos idzie do piekla to tam liczenie kolejnych pochodnych jest jedna z tortur
 {
-	return 0;
+        const double t = pow( x * x - 1, -0.5) * x;
+        const double b = sqrt(x * x - 1);
+
+        // podzielone na dwa wyrazenia, bo roznia sie tylko kilkoma znakami
+        const double w1 = (st - 1) * pow( x + b, st - 2 ) * (1 + t) * (1 + t) + pow( x + b, st - 1 ) * ( -0.5 * pow( x * x - 1, -1.5 ) * 2 * x * x + pow( x * x - 1, -0.5 ) );
+        const double w2 = (st - 1) * pow( x - b, st - 2 ) * (1 - t) * (1 - t) + pow( x - b, st - 1 ) * (  0.5 * pow( x * x - 1, -1.5 ) * 2 * x * x - pow( x * x - 1, -0.5 ) );
+
+        return 0.5 * st * (w1 + w2);
 }
 
 double d_czen_trzy(int st, double x)
 {
-	return 0;
+        const double a = sqrt(x*x - 1);
+
+        const double licznik = pow( x - a, st) * ( 3 * st * x * a + (st*st + 2) * x * x - st * st + 1 ) + pow( x + a, st) * ( 3 * st * x * a + (-1 * st * st - 2)* x * x + st*st - 1 );
+
+        const double mianownik = 2 * pow( x*x - 1, 2.5);
+
+
+        return -st * licznik / mianownik;
 }
+
--- lmp10/projekt/test.c
+++ lmp10/projekt/test.c
@@ -5,24 +5,39 @@ double cze_n(int st, double x)
 {
         if(st == 0) return 1;
         else if( st == 1) return x;
-        else  return 2*x*cze_n(st-1,x)-cze_n(st-2,x);
+        else  return 2 * x * cze_n(st-1, x) - cze_n(st-2, x);
 }
 
 double d_czen_jeden(int st, double x)
 {
-        if(st == 0) return 0;
-        else if( st == 1) return 1;
-        else
-        {
-                return (2*cze_n(st-1,x) + 2*x*d_czen_jeden(st-1,x)-d_czen_jeden(st-2,x));
-        }
+        return 0.5 * st * ( pow( x + pow(x * x - 1, 0.5) , st - 1 ) * ( 1 + x * pow(x * x - 1, -0.5) ) + pow( x - pow(x * x - 1, 0.5) , st - 1 ) * ( 1 - x * pow(x * x - 1, -0.5) ) );
+
 }
 
-
-double d2(int st, double x)
+double d_czen_dwa(int st, double x) // jak ktos idzie do piekla to tam liczenie kolejnych pochodnych jest jedna z tortur
 {
-	return st/2*( pow( x + pow( x*x -1, 1/2) , st - 1 ) * ( 1 + x * pow(x*x-1, -1/2) ) + pow( x - pow( x*x -1, 1/2) , st - 1 ) * ( 1 - x * pow(x*x-1, -1/2) ) );
+        const double t = pow( x * x - 1, -0.5) * x;
+        const double b = sqrt(x * x - 1);
+
+        // podzielone na dwa wyrazenia, bo roznia sie tylko kilkoma znakami
+        const double w1 = (st - 1) * pow( x + b, st - 2 ) * (1 + t) * (1 + t) + pow( x + b, st - 1 ) * ( -0.5 * pow( x * x - 1, -1.5 ) * 2 * x * x + pow( x * x - 1, -0.5 ) );
+        const double w2 = (st - 1) * pow( x - b, st - 2 ) * (1 - t) * (1 - t) + pow( x - b, st - 1 ) * (  0.5 * pow( x * x - 1, -1.5 ) * 2 * x * x - pow( x * x - 1, -0.5 ) );
+
+        return 0.5 * st * (w1 + w2);
 }
+
+double d_czen_trzy(int st, double x)
+{
+        const double a = sqrt(x*x - 1);
+
+        const double licznik = pow( x - a, st) * ( 3 * st * x * a + (st*st + 2) * x * x - st * st + 1 ) + pow( x + a, st) * ( 3 * st * x * a + (-1 * st * st - 2)* x * x + st*st - 1 );
+
+        const double mianownik = 2 * pow( x*x - 1, 2.5);
+
+
+        return -st * licznik / mianownik;
+}
+
 
 int main()
 {
@@ -30,7 +45,7 @@ int main()
 	int s = 3;
 	double x = 5.4;
 
-	printf("\t %lf \n\t %lf\n",d_czen_jeden(s,x),d2(s,x)); 
+	printf("pierwsza:  %lf \ndruga:  %lf\ntrzecia:  %lf\n", d_czen_jeden(s,x), d_czen_dwa(s,x), d_czen_trzy(s,x) ); 
 
 	return 0;
 }
